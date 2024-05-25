e4726791e3e1ffe3cbf28de9fe3961d729d1c39b
Rahul N L <45726796+rahul-nl@users.noreply.github.com>
2024-05-25 10:36:45 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2">1); //true
+// console.log("02">1); //true
+
+console.log(null >0) //false
+console.log(null ==0) //false
+console.log(null >=0) //true
+
+console.log(undefined==0 ); //false for all comparison
+
+
+//  ===   strict check
+
+
+console.log("2"===2);
