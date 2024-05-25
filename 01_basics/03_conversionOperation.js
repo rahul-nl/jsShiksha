a5762bb18f197ddfe22daed43b2cd5dcf687dddc
Rahul N L <45726796+rahul-nl@users.noreply.github.com>
2024-05-25 10:30:38 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 -> true; 0 -> false
 //""-> false
@@ -28,3 +28,30 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
+
+
+
+
+
+// **************************Operations**************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " rahul"
+let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
